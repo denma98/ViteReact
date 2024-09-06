90237fe2b0ed62e0720a30d3f7470616bb51010e
Denma <deepak21319@iiitd.ac.in>
2024-09-06 23:04:07 +0530

    custom hook

---
+++ currencyProj/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-orange-500'> Currency Calculator</div>
+    </>
+  )
+}
+
+export default App
