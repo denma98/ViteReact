757fc96271d0049c39d6f370b33e0f016c7e143b
Denma <deepak21319@iiitd.ac.in>
2024-09-07 14:55:39 +0530

    Added header, footer, home page

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import About from './Components/About/About.jsx'
+import Home from './Components/Home/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '', // after the slash to mtlb localhost:3000/ k baad kuch nahi hoga
+        element: <Home/>
+      },
+      {
+        path: 'about', // localhost:3000/about
+        element: <About/>
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
