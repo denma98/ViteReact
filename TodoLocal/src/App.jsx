d8e0a4aa7c347bac6861f4ea131cd2537bb05c1f
Denma <deepak21319@iiitd.ac.in>
2024-09-09 00:26:04 +0530

    check TodoForm again, quite complex

--- TodoLocal/src/App.jsx
+++ TodoLocal/src/App.jsx
@@ -1,36 +1,47 @@
-import { useState } from 'react'
-import {TodoProvider} from './Context'
+import { useEffect, useState } from 'react'
+import { TodoProvider } from './Context'
 
 function App() {
-  const [todos,setTodos] = useState([])
-  const addTodo = (todo) =>{
-    setTodos((prev) => [{id: Date.now() ,...todo}, ...prev])
+  const [todos, setTodos] = useState([])
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev])
   }
-  const editTodo = (id, todo) =>{
-    setTodos( (prev) => prev.map((prevTodo) => prevTodo.id === id ? todo: prevTodo ) ) // if id matches then return todo else return prevTodo
+  const editTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo)) // if id matches then return todo else return prevTodo
   }
 
   const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((todo) => todo.id !== id) ) // only keeping the todos whose id is not equal to the id passed
+    setTodos((prev) => prev.filter((todo) => todo.id !== id)) // only keeping the todos whose id is not equal to the id passed
   }
 
   const toggleDone = (id) => {
-    setTodos((prev) => prev.map((todo) => todo.id === id ? {...prev, isDone: !prev.isDone } : todo  ))
+    setTodos((prev) => prev.map((todo) => todo.id === id ? { ...prev, isDone: !prev.isDone } : todo))
   }
 
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if (todos && todos.length) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
   return (
-    <TodoProvider value={{todo, editTodo, addTodo, toggleDone, deleteTodo}}>
+    <TodoProvider value={{ todo, editTodo, addTodo, toggleDone, deleteTodo }}>
       <div className="bg-[#172842] min-h-screen py-8">
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-                    <div className="mb-4">
-                        {/* Todo form goes here */} 
-                    </div>
-                    <div className="flex flex-wrap gap-y-3">
-                        {/*Loop and Add TodoItem here */}
-                    </div>
-                </div>
-            </div>
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
     </TodoProvider>
   )
 }
