05b8efcdcf3c10f656361192b614ca8584832189
Denma <deepak21319@iiitd.ac.in>
2024-09-08 23:51:45 +0530

    context is done here. now we will do local storage

--- TodoLocal/src/App.jsx
+++ TodoLocal/src/App.jsx
@@ -2,13 +2,35 @@ import { useState } from 'react'
 import {TodoProvider} from './Context'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos,setTodos] = useState([])
+  const addTodo = (todo) =>{
+    setTodos((prev) => [{id: Date.now() ,...todo}, ...prev])
+  }
+  const editTodo = (id, todo) =>{
+    setTodos( (prev) => prev.map((prevTodo) => prevTodo.id === id ? todo: prevTodo ) ) // if id matches then return todo else return prevTodo
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id) ) // only keeping the todos whose id is not equal to the id passed
+  }
+
+  const toggleDone = (id) => {
+    setTodos((prev) => prev.map((todo) => todo.id === id ? {...prev, isDone: !prev.isDone } : todo  ))
+  }
 
   return (
     <TodoProvider value={{todo, editTodo, addTodo, toggleDone, deleteTodo}}>
-      <h1 className="text-3xl font-bold underline bg-slate-500">
-        Hello world!
-      </h1>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
     </TodoProvider>
   )
 }
